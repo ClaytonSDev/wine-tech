05396d13915923bc47733d6f7760d7e06282d409
ClaytonSDev <claytonsalesdev@gmail.com>
2025-09-17 21:01:59 -0300

    Corrige base para deploy na Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/wine-tech/", // 👈 necessário para GitHub Pages
+  base: "/", // 👈 ideal para Vercel
   plugins: [react()],
   server: {
     port: 5173,
