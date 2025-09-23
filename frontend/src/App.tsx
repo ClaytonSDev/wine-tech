4232d52ad4835f45a9d2a32f3032f5418a4e5557
ClaytonSDev <claytonsalesdev@gmail.com>
2025-09-23 14:05:41 -0300

    Adiciona import do whatsappIcon corrigido

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import styled from "styled-components";
-
 import Home from "./pages/Home";
 import Sobre from "./pages/Sobre";
 import Servicos from "./pages/Servicos";
