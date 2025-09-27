2f528c5787d2c65374a1c0c7f1a53ef427e4193c
ClaytonSDev <claytonsalesdev@gmail.com>
2025-09-27 01:13:37 -0300

    Atualização geral: alterações, exclusões e novos arquivos

--- backend/src/main/java/com/winetech/WinetechBackendApplication.java
+++ backend/src/main/java/com/winetech/WinetechBackendApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WinetechBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WinetechBackendApplication.class, args);
 	}
-
 }
--- backend/src/main/java/com/winetech/repository/ContatoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.winetech.repository;
-
-import com.winetech.model.Contato;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContatoRepository extends JpaRepository<Contato, Long> {
-}
